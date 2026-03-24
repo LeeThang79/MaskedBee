fd7e542b9adf6927aa4a051b4d7e3031d25b4ab9
Thảo Hoàng <echxanh123yaya@gmail.com>
2026-03-24 18:02:47 +0700

    room1

---
+++ assets/map/wallfloor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="wallfloor" tilewidth="32" tileheight="32" tilecount="300" columns="20" objectalignment="topleft">
+ <image source="real_tileset.png" width="640" height="480"/>
+</tileset>
---
+++ assets/map/wax_texture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="wax_texture" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="wax_texture.png" width="288" height="48"/>
+</tileset>
