d74f4397b30250b7d3cf03593f42c7a5549802c9
Thảo Hoàng <echxanh123yaya@gmail.com>
2026-03-30 11:48:10 +0700

    map

--- assets/map/bookself.tsx
+++ assets/map/bookself.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="bookself" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="bookself.png" width="272" height="272"/>
+<tileset version="1.10" tiledversion="1.12.0" name="bookself" tilewidth="32" tileheight="32" tilecount="210" columns="14">
+ <image source="bookself.png" width="450" height="500"/>
 </tileset>
