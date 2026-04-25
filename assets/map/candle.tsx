b5457e3af12764a20457ec343d257a34e7df704e
Thảo Hoàng <echxanh123yaya@gmail.com>
2026-04-25 21:18:22 +0700

    25/4 map

---
+++ assets/map/candle.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="candle" tilewidth="32" tileheight="32" tilecount="56" columns="7">
+ <image source="candle.png" width="224" height="256"/>
+ <tile id="35">
+  <animation>
+   <frame tileid="28" duration="400"/>
+   <frame tileid="21" duration="400"/>
+   <frame tileid="14" duration="400"/>
+   <frame tileid="7" duration="400"/>
+   <frame tileid="28" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="7" duration="400"/>
+   <frame tileid="14" duration="400"/>
+   <frame tileid="21" duration="400"/>
+   <frame tileid="28" duration="400"/>
+   <frame tileid="28" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="39">
+  <animation>
+   <frame tileid="32" duration="400"/>
+   <frame tileid="18" duration="400"/>
+   <frame tileid="25" duration="400"/>
+   <frame tileid="32" duration="400"/>
+   <frame tileid="28" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="41">
+  <animation>
+   <frame tileid="27" duration="400"/>
+   <frame tileid="34" duration="400"/>
+   <frame tileid="27" duration="400"/>
+   <frame tileid="27" duration="400"/>
+   <frame tileid="28" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
