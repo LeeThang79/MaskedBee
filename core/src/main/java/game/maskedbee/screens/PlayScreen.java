c8c28770843af7ef6486ab32c6e88371508d7582
staif <tailuffy4@gmail.com>
2026-06-06 23:24:30 +0700

    Update boss logic and game progress

--- core/src/main/java/game/maskedbee/screens/PlayScreen.java
+++ core/src/main/java/game/maskedbee/screens/PlayScreen.java
@@ -36,7 +36,7 @@ public class PlayScreen implements Screen {
     public final CORE game;
     public Player myPlayer;
 
-    private static final String START_MAP = "Holding_Chamber.tmx";
+    private static final String START_MAP = "Old_Corridor.tmx";
 
     private OrthographicCamera camera;
     private Viewport viewport;
